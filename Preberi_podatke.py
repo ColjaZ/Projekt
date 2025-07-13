7e9e6c76aed4c56963bc21c781770d27d806b844
ColjaZ <zc33406@student.uni-lj.si>
2025-07-13 18:03:29 +0200

    zapis v csv

--- Preberi_podatke.py
+++ Preberi_podatke.py
@@ -1,14 +1,14 @@
 import re
 
-with open("(highest-score)-stran-1.html", encoding="utf-8") as f:
-    vsebina = f.read()
+# with open("(highest-score)-stran-1.html", encoding="utf-8") as f:
+#     vsebina = f.read()
 
 vzorec_bloka = re.compile(r'(?=<div[^>]+id="question-summary-\d+")')
-bloki = vzorec_bloka.split(vsebina) #splita vse do prvega takega vzorca (bloke se ustvari kasneje i think ko poklicem to funkcijo al neki)
-bloki = bloki[1:] #znebimo se prvega bloka ki ne predstavlja nobenega vprašanja
+# bloki = vzorec_bloka.split(vsebina) #splita vse do prvega takega vzorca (bloke se ustvari kasneje i think ko poklicem to funkcijo al neki)
+# bloki = bloki[1:] #znebimo se prvega bloka ki ne predstavlja nobenega vprašanja
 
 vzorec_nesprejeti = re.compile(
-    r'<div[^>]+id="question-summary-(?P<id>\d+)"[^>]*>.*?' 
+    r'<div[^>]+id="question-summary-(?P<id>\d+)"[^>]*>.*?'
     r'<span[^>]+stats-item-number">(?P<votes>-?\d+)</span>.*?'
     r'<div[^>]+title="(?P<answers>\d+)\s+answers?".*?'
     r'<div[^>]+title="(?P<views>[\d,]+k?)\s+views".*?'
@@ -25,18 +25,18 @@ vzorec_sprejeti = re.compile(
     flags=re.DOTALL
 )
 
-vzorec_tag = re.compile(r'<a[^>]+class="[^"]*\bpost-tag\b[^"]*"[^>]*>([^<]+)</a>')
+vzorec_oznaka = re.compile(r'<a[^>]+class="[^"]*\bpost-tag\b[^"]*"[^>]*>([^<]+)</a>')
 
-vzorec_author = re.compile(
+vzorec_avtor = re.compile(
     r'<a href="/users/(?P<user_id>\d+)/[^"]+"[^>]*class="flex--item">'
     r'(?P<author>[^<]+)</a>'
 )
 
-vzorec_time = re.compile(r"<span[^>]+class='relativetime'[^>]*>(?P<time>[^<]+)</span>")
+vzorec_cas = re.compile(r"<span[^>]+class='relativetime'[^>]*>(?P<time>[^<]+)</span>")
 
 vzorec_sprejetodg = re.compile(r'\bhas-accepted-answer\b')
 
-vzorec_reputation = re.compile(
+vzorec_ugled = re.compile(
     r'<li[^>]+class="s-user-card--rep"[^>]*>.*?'
     r'<span[^>]*>(?P<rep>[\d,]+)</span>',
     flags=re.DOTALL
@@ -44,21 +44,18 @@ vzorec_reputation = re.compile(
 
 def izloci_podatke_vprasanja(blok):
     m = vzorec_sprejeti.search(blok) or vzorec_nesprejeti.search(blok) #drugacna html koda za sprejete odgovore vs brez sprejetih odgovorov
-    info = m.groupdict()
-    info['id'] = int(info['id'])
-    info['votes']   = int(info['votes'])
-    info['answers'] = int(info['answers'])
-    v = info['views'].lower().replace(',','')
-    info['views'] = int(float(v[:-1])*1000) if v.endswith('k') else int(v)
-    info['tags'] = vzorec_tag.findall(blok)
-    author = vzorec_author.search(blok)
-    if author: #preverim da ne bo samo prazen
-        info["author"] = author.group("author")
-    reputation = vzorec_reputation.search(blok)
-    if reputation:
-        info['reputation'] = int(reputation.group("rep").replace(",", ""))
-    info['has_accepted_answer'] = bool(vzorec_sprejetodg.search(blok))
-    time = vzorec_time.search(blok)
-    if time:
-        info["time"] = time.group("time")
-    return info
+    vprasanje = m.groupdict()
+    vprasanje['id'] = int(vprasanje['id'])
+    vprasanje['votes']   = int(vprasanje['votes'])
+    vprasanje['answers'] = int(vprasanje['answers'])
+    v = vprasanje['views'].lower().replace(',','')
+    vprasanje['views'] = int(float(v[:-1])*1000) if v.endswith('k') else int(v)
+    vprasanje['tags'] = vzorec_oznaka.findall(blok)
+    author = vzorec_avtor.search(blok)
+    vprasanje["author"] = author.group("author") if author else None
+    reputation = vzorec_ugled.search(blok)
+    vprasanje['reputation'] = int(reputation.group("rep").replace(",", "")) if reputation else None
+    vprasanje['has_accepted_answer'] = bool(vzorec_sprejetodg.search(blok))
+    time = vzorec_cas.search(blok)
+    vprasanje["time"] = time.group("time") if time else None
+    return vprasanje
--- pobiranje_podatkov.py
+++ pobiranje_podatkov.py
@@ -1,52 +1,99 @@
 import requests
 import time
 
-for i in range(1,3):
-    link = f"https://math.stackexchange.com/questions?tab=votes&page={i}"
-    odziv = requests.get(link)
+# for i in range(1,3):
+#     link = f"https://math.stackexchange.com/questions?tab=votes&page={i}"
+#     odziv = requests.get(link)
 
-    time.sleep(2)
+#     time.sleep(2)
 
-    if odziv.status_code == 200:
-        with open(f"(highest-score)-stran-{i}.html", "w", encoding="utf-8") as f:
-            f.write(odziv.text)
-    else:
-        print("napaka")
+#     if odziv.status_code == 200:
+#         with open(f"(highest-score)-stran-{i}.html", "w", encoding="utf-8") as f:
+#             f.write(odziv.text)
+#     else:
+#         print("napaka")
 
-for i in range(1,3):
-    link = f"https://math.stackexchange.com/questions?tab=trending&page={i}"
-    odziv = requests.get(link)
+# for i in range(1,3):
+#     link = f"https://math.stackexchange.com/questions?tab=trending&page={i}"
+#     odziv = requests.get(link)
 
-    time.sleep(2)
+#     time.sleep(2)
 
-    if odziv.status_code == 200:
-        with open(f"(trending)-stran-{i}.html", "w", encoding="utf-8") as f:
-            f.write(odziv.text)
-    else:
-        print("napaka")
+#     if odziv.status_code == 200:
+#         with open(f"(trending)-stran-{i}.html", "w", encoding="utf-8") as f:
+#             f.write(odziv.text)
+#     else:
+#         print("napaka")
 
 
-for i in range(1,3):
-    link = f"https://math.stackexchange.com/unanswered/tagged/?page={i}&tab=votes&pagesize=50"
-    odziv = requests.get(link)
+# for i in range(1,3):
+#     link = f"https://math.stackexchange.com/unanswered/tagged/?page={i}&tab=votes&pagesize=50"
+#     odziv = requests.get(link)
 
-    time.sleep(2)
+#     time.sleep(2)
 
-    if odziv.status_code == 200:
-        with open(f"(unanswered)-stran-{i}.html", "w", encoding="utf-8") as f:
-            f.write(odziv.text)
-    else:
-        print("napaka")
+#     if odziv.status_code == 200:
+#         with open(f"(unanswered)-stran-{i}.html", "w", encoding="utf-8") as f:
+#             f.write(odziv.text)
+#     else:
+#         print("napaka")
 
 from Preberi_podatke import vzorec_bloka, izloci_podatke_vprasanja
+import json
+import csv
 
 vprasanja = []
-for i in range(1,3):
-    with open(f"(highest-score)-stran-{i}.html", encoding="utf-8") as f:
-        vsebina = f.read()
+for type in ["highest-score", "trending","unanswered"]:
+    for i in range(1,3):
+        with open(f"({type})-stran-{i}.html", encoding="utf-8") as f:
+            vsebina = f.read()
 
-    bloki = vzorec_bloka.split(vsebina)[1:] #splita in odstrani vse pred prvim vprasanjem
-    for blok in bloki:
-        vprasanje = izloci_podatke_vprasanja(blok)
-        vprasanja.append(vprasanje)
-print(vprasanja)
+        bloki = vzorec_bloka.split(vsebina)[1:] #splita in odstrani vse pred prvim vprasanjem
+        for blok in bloki:
+            vprasanje = izloci_podatke_vprasanja(blok)
+            vprasanje["kategorija"] = type
+            vprasanja.append(vprasanje)
+
+# with open("vprasanja.json", "w", encoding="utf-8") as f:
+#     json.dump(vprasanja, f, ensure_ascii=False, indent=4)
+avtorji = {}
+oznake = set()
+oznake_vprasanja = []
+with open("vprasanja.csv", "w", encoding="utf-8") as f:
+    pisatelj = csv.writer(f)
+    pisatelj.writerow(["Kategorija", "id", "Naslov", "Ogledi", "Glasovi", "Število odgovorov", "Ima sprejet odgovor", "Avtor", "Čas objave"])
+    for vprasanje in vprasanja:
+        kategorija = vprasanje["kategorija"]
+        id = vprasanje["id"]
+        title = vprasanje["title"]
+        views = vprasanje["views"]
+        votes = vprasanje["votes"]
+        answers = vprasanje["answers"]
+        accepted_answer = vprasanje["has_accepted_answer"]
+        author = vprasanje["author"]
+        cas = vprasanje["time"]
+        pisatelj.writerow([kategorija,id,title,views,votes,answers,accepted_answer,author,cas])
+        if vprasanje.get("author") is not None:
+            avtorji[vprasanje["author"]] = vprasanje.get("reputation", 0)
+        for oznaka in vprasanje["tags"]:
+            oznake.add(oznaka)
+            oznake_vprasanja.append((vprasanje["id"], oznaka))
+
+with open("avtorji.csv", "w", encoding="utf-8") as f:
+    pisatelj = csv.writer(f)
+    pisatelj.writerow(["author", "reputation"])
+    for author, rep in avtorji.items():
+        pisatelj.writerow([author, rep])
+
+with open("oznake.csv", "w", encoding="utf-8") as f:
+    pisatelj = csv.writer(f)
+    pisatelj.writerow(["oznaka"])
+    for oznaka in oznake:
+        pisatelj.writerow([oznaka])
+
+with open("oznake_vprasanj.csv", "w", encoding="utf-8") as f:
+    pisatelj = csv.writer(f)
+    pisatelj.writerow(["id", "oznaka"])
+    for id, oznaka in oznake_vprasanja:
+        pisatelj.writerow([id, oznaka])
+
